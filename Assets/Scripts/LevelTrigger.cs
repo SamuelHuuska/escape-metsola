3a15521b8d020753f87d74647c82069afd6718c5
Rasmus-Ko <frako002@edu.xamk.fi>
2024-10-13 16:52:55 +0300

    backup 2

---
+++ Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTrigger : MonoBehaviour
+{
+    [SerializeField] private CollectibleCount collectibleCount; // Reference to the CollectibleCount script
+    private const int requiredScore = 5; // The score required to unlock the next level
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Ensure the collider is the player
+        {
+            if (collectibleCount.GetScore() >= requiredScore)
+            {
+                // Load the scene named "End"
+                SceneManager.LoadScene("End");
+            }
+            else
+            {
+                Debug.Log("Collect more items to unlock the next level!");
+            }
+        }
+    }
+}
